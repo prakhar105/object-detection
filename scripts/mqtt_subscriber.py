ca05fbed31b055e79f1f6d26757a82f98faa195d
prakhar105 <prakhar.awasthi105@gmail.com>
2025-07-07 21:27:28 +0930

    Added argsparse in script files

--- scripts/mqtt_subscriber.py
+++ scripts/mqtt_subscriber.py
@@ -2,6 +2,13 @@ import cv2
 import base64
 import numpy as np
 import paho.mqtt.client as mqtt
+import argparse
+
+parser=argparse.ArgumentParser(description="Object detection MQTT")
+parser.add_argument("--ip",type=str, required=True, help="IP address of Tailscale machine")
+parser.add_argument('--port', type=int, default=1883, help='Port of MQTT broker (default: 1883)')
+args = parser.parse_args()
+
 
 def on_connect(client, userdata, flags, rc):
     client.subscribe("video/stream")
@@ -19,5 +26,5 @@ client = mqtt.Client()
 client.on_connect = on_connect
 client.on_message = on_message
 
-client.connect("localhost", 1883, 60)
+client.connect(args.ip, args.port, 60)
 client.loop_forever()
--- scripts/realtime_video_detect_mqtt.py
+++ scripts/realtime_video_detect_mqtt.py
@@ -3,10 +3,18 @@ import base64
 import paho.mqtt.client as mqtt
 from ultralytics import YOLO
 import time
+import argparse
 
+
+
+#----------Argument parser------------------
+parser=argparse.ArgumentParser(description="Object detection MQTT")
+parser.add_argument("--ip",type=str, required=True, help="IP address of Tailscale machine")
+parser.add_argument('--port', type=int, default=1883, help='Port of MQTT broker (default: 1883)')
+args = parser.parse_args()
 # MQTT setup
-MQTT_BROKER = "localhost"  # Replace with your broker's IP or domain
-MQTT_PORT = 1883
+MQTT_BROKER = args.ip  # Replace with your broker's IP or domain
+MQTT_PORT = args.port
 MQTT_TOPIC = "video/stream"
 
 client = mqtt.Client()
